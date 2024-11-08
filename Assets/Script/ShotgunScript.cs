6a71bd7d60bc2d50e85c644acee81294a42a7edd
PERRET Léandre <leandreperret@gmail.com>
2024-11-08 16:04:11 +0100

    First Weapon Made

---
+++ Assets/Script/ShotgunScript.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShotgunScript : WeaponScript
+{
+    protected override void WeaponConfigure()
+    {
+        _ammo = 8;
+        _cd = 2f;
+        _currentCd = 0f;
+        _bulletSpeed = 1f;
+        _bulletDamage = 1f;
+        _bulletLifeTime = 3f;
+    }
+
+    protected override void Shot()
+    {
+        GameObject bullet = Instantiate(_bullet);
+        //Physics.IgnoreCollision(bullet.GetComponent<Collider>(), GetComponent<Collider>());
+        bullet.transform.position = transform.GetChild(0).position;
+        Vector3 rotation = bullet.transform.rotation.eulerAngles;
+        bullet.transform.rotation = Quaternion.Euler(rotation.x, transform.eulerAngles.y, rotation.z);
+        bullet.GetComponent<Rigidbody>().AddForce(transform.GetChild(0).forward * _bulletSpeed, ForceMode.Impulse);
+        _currentCd = _cd;
+    }
+}
--- Assets/Script/WeaponScript.cs
+++ Assets/Script/WeaponScript.cs
@@ -1,17 +1,65 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class WeaponScript : MonoBehaviour
 {
+    public GameObject _bullet;
+    protected PlayerInput _input;
+    protected InputAction _actionFire;
+    protected int _ammo;
+    protected float _cd;
+    protected float _bulletSpeed;
+    protected float _bulletDamage;
+    protected float _bulletLifeTime;
+    protected float _currentCd;
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+        _input = new PlayerInput();
+        WeaponConfigure();
+    }
+
+    private void OnEnable()
+    {
+        _actionFire = _input.Player.Fire;
+        _actionFire.Enable();
+    }
+
+    private void OnDisable()
+    {
+        _actionFire.Disable();
     }
 
     // Update is called once per frame
     void Update()
+    {
+        if (_actionFire.IsPressed() && GotAmmo() && _currentCd == 0)
+        {
+            Shot();
+        }
+
+        if (_currentCd != 0)
+            _currentCd -= Time.deltaTime;
+
+        if (_currentCd < 0)
+            _currentCd = 0;
+    }
+
+    bool GotAmmo()
+    {
+        if (_ammo > 0)
+            return true;
+        return false;
+    }
+
+    virtual protected void WeaponConfigure()
+    { 
+
+    }
+
+    virtual protected void Shot()
     {
         
     }
